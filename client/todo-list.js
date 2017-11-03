b2429bb38413eeda4de72c822c0af4cbab0ca553
Alexander Lee <alex8usa8@gmail.com>
2017-11-03 16:18:53 -0700

    NO MORE BULLETS! #10

--- client/todo-list.js
+++ client/todo-list.js
@@ -1,13 +1,25 @@
 import React from 'react'
+
+const listEntryStyle = {
+  'box-shadow': '0 3px 5px black',
+  'background-color': 'rgb(225, 225, 200)',
+  'border-radius': '15px',
+  'padding': '5px',
+  'margin': '8px'
+}
 const renderTodoItem = ({ task, dueDate }, index) =>
-  <li key={ index }>
+  <li style={ listEntryStyle } key={ index }>
     <h5>Task: { task }</h5>
     <h5>Due: { dueDate }</h5>
   </li>
 
+const listStyle = {
+  'listStyle': 'none',
+  'padding': 2
+}
 export default function TodoList({ todos }) {
   return (
-    <ul>
+    <ul style={ listStyle }>
       { todos.map(renderTodoItem) }
     </ul>
   )
