809ef98a8dec3f3a3fc1587ba47bf2ca0a37a560
Alexander Lee <alex8usa8@gmail.com>
2017-11-03 13:29:50 -0700

    Corrected myself in extract an App Container Component from TodoForm component that renders the TodoForm. #10

--- client/app.js
+++ client/app.js
@@ -1,29 +1,9 @@
 import React, { Component } from 'react'
+import TodoForm from './todo-form.js'
 export default class App extends Component {
   render() {
     return (
-      <form id="signUpForm" onSubmit={ this.handleSubmit }>
-        <div className="form-group">
-          <label htmlFor="task">Task</label>
-          <input
-            type="text"
-            className="form-control"
-            id="taskInput"
-            name="taskInput"
-            aria-describedby="emailHelp"
-            placeholder="Enter task" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="dueDate">Date</label>
-          <input
-            type="tesxt"
-            className="form-control"
-            id="dueDateInput"
-            name="dueDateInput"
-            placeholder="Enter Due Date" />
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+      <TodoForm />
     )
   }
 }
