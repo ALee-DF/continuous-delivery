4d2ef765d5ae5fdd41e4b7d8463d0d74fa5ca06f
Alexander Lee <alex8usa8@gmail.com>
2017-11-03 14:14:29 -0700

    Move POSTING behavior from TodoForm to App and pass it as a prop to TodoForm. #10

--- client/app.js
+++ client/app.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
 import TodoForm from './todo-form.js'
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { todos: [] }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const data = {
+      task: formData.get('taskInput'),
+      dueDate: formData.get('dueDateInput')
+    }
+    fetch('/api/todos/', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    event.target.reset()
+  }
+
   render() {
     return (
-      <TodoForm />
+      <TodoForm handleSubmit={ this.handleSubmit }/>
     )
   }
 }
